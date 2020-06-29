1a0f818301765f1d7f123e4d95dce5fea3e8a857
Jędrzej <jedrzejb123@gmail.com>
2020-06-29 09:18:17 +0200

    styling

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+const mainBtn = document.querySelector(".main__btn");
+let mainHeight = document.querySelector("main").clientHeight;
+
 function parallax(element, distance, speed) {
-    const item = document.querySelector(element);
-    item.style.transform = `translateY(${distance * speed}px)`;
+  const item = document.querySelector(element);
+  item.style.transform = `translateY(${distance * speed}px)`;
 }
 window.addEventListener("scroll", function () {
-    parallax("h1", window.scrollY, 0.1);
-    parallax(".main__bg3", window.scrollY, 0.1);
-    parallax(".main__bg2", window.scrollY, 0.1);
-    parallax(".main__bg1", window.scrollY, 0.2);
-})
+  parallax("h1", window.scrollY, 0.4);
+  parallax(".main__bg3", window.scrollY, 0.2);
+  parallax(".main__bg2", window.scrollY, 0.3);
+  parallax(".main__bg1", window.scrollY, 0.5);
+  parallax(".main__circle--wrapper", window.scrollY, 0.4);
+});
+
+mainBtn.addEventListener("click", () =>
+  window.scrollTo({
+    top: mainHeight,
+    behavior: "smooth",
+  })
+);
